b12d6991ad07575244cec6f5a433400ea8767ff9
Kojiro Yokota <macneko.ayu@gmail.com>
2018-10-17 14:04:57 +0900

    Add bridging header

---
+++ CardIOScanSample/CardIOScanSample-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  CardIOScanSample-Bridging-Header.h
+//  CardIOScanSample
+//
+//  Created by Kojiro Yokota on 2018/10/17.
+//  Copyright © 2018年 macneko. All rights reserved.
+//
+
+#ifndef CardIOScanSample_Bridging_Header_h
+#define CardIOScanSample_Bridging_Header_h
+
+#import "CardIO.h"
+
+#endif /* CardIOScanSample_Bridging_Header_h */
