cb1b77de5baa0be70d5b940249258845f5891142
Kojiro Yokota <macneko.ayu@gmail.com>
2018-10-17 13:59:19 +0900

    Add project files

---
+++ CardIOScanSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CardIOScanSample
+//
+//  Created by Kojiro Yokota on 2018/10/16.
+//  Copyright © 2018年 macneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
